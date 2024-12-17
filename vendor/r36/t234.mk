adf6aaac4216e84e3dbd4576d6de572361890c9e
Aaron Kling <webgeek1234@gmail.com>
2024-12-16 22:03:49 -0600

    vendor: Use copy-files for firmware targets
    There's no real build time checks that the build system can do for these
    type of files. Plus duplicate copy-files entries are now a fatal error,
    so there's no benefit to keeping these as modules.
    
    Change-Id: Id3094f60381193ec652d3320270320959c42fb4d

--- vendor/r36/t234.mk
+++ vendor/r36/t234.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,36 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/nvidia/t234-common/vendor
+T234_FIRMWARE_PATH := vendor/nvidia/t234/r36/firmware
 
 # GPU firmware
-PRODUCT_PACKAGES += \
-    acr-gsp.data.encrypt.bin.prod \
-    acr-gsp.manifest.encrypt.bin.out.bin.prod \
-    acr-gsp.text.encrypt.bin.prod \
-    fecs_encrypt_prod \
-    fecs_pkc_sig_encrypt \
-    gpccs_encrypt_prod \
-    gpccs_pkc_sig_encrypt \
-    gpmu_ucode_next_prod_desc \
-    gpmu_ucode_next_prod_image \
-    NETA_img_prod_encrypted \
-    NETB_img_prod_encrypted \
-    NETC_img_prod_encrypted \
-    NETD_img_prod_encrypted \
-    pmu_pkc_prod_sig \
-    safety-scheduler.data.encrypt.bin.prod \
-    safety-scheduler.manifest.encrypt.bin.out.bin.prod \
-    safety-scheduler.text.encrypt.bin.prod
+PRODUCT_COPY_FILES += \
+    $(T234_FIRMWARE_PATH)/ga10b/acr-gsp.data.encrypt.bin.prod:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/acr-gsp.data.encrypt.bin.prod \
+    $(T234_FIRMWARE_PATH)/ga10b/acr-gsp.manifest.encrypt.bin.out.bin.prod:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/acr-gsp.manifest.encrypt.bin.out.bin.prod \
+    $(T234_FIRMWARE_PATH)/ga10b/acr-gsp.text.encrypt.bin.prod:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/acr-gsp.text.encrypt.bin.prod \
+    $(T234_FIRMWARE_PATH)/ga10b/fecs_encrypt_prod.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/fecs_encrypt_prod.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/fecs_pkc_sig_encrypt.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/fecs_pkc_sig_encrypt.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/gpccs_encrypt_prod.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/gpccs_encrypt_prod.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/gpccs_pkc_sig_encrypt.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/gpccs_pkc_sig_encrypt.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/gpmu_ucode_next_prod_desc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/gpmu_ucode_next_prod_desc.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/gpmu_ucode_next_prod_image.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/gpmu_ucode_next_prod_image.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/NETA_img_prod_encrypted.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/NETA_img_prod_encrypted.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/NETB_img_prod_encrypted.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/NETB_img_prod_encrypted.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/NETC_img_prod_encrypted.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/NETC_img_prod_encrypted.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/NETD_img_prod_encrypted.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/NETD_img_prod_encrypted.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/pmu_pkc_prod_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/pmu_pkc_prod_sig.bin \
+    $(T234_FIRMWARE_PATH)/ga10b/safety-scheduler.data.encrypt.bin.prod:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/safety-scheduler.data.encrypt.bin.prod \
+    $(T234_FIRMWARE_PATH)/ga10b/safety-scheduler.manifest.encrypt.bin.out.bin.prod:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/safety-scheduler.manifest.encrypt.bin.out.bin.prod \
+    $(T234_FIRMWARE_PATH)/ga10b/safety-scheduler.text.encrypt.bin.prod:$(TARGET_COPY_OUT_VENDOR)/firmware/ga10b/safety-scheduler.text.encrypt.bin.prod
 
 # General firmware
-PRODUCT_PACKAGES += \
-    nvhost_nvdec050_desc_prod \
-    nvhost_nvdla020 \
-    nvhost_nvenc080 \
-    nvhost_nvjpg013 \
-    nvhost_ofa012 \
-    nvhost_tsec_desc \
-    nvhost_tsec_riscv \
-    nvhost_vic042 \
-    nvpva_020
+PRODUCT_COPY_FILES += \
+    $(T234_FIRMWARE_PATH)/tegra23x/nvhost_nvdec050_desc_prod.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra23x/nvhost_nvdec050_desc_prod.bin \
+    $(T234_FIRMWARE_PATH)/nvhost_nvdla020.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvhost_nvdla020.fw \
+    $(T234_FIRMWARE_PATH)/tegra23x/nvhost_nvenc080.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra23x/nvhost_nvenc080.fw \
+    $(T234_FIRMWARE_PATH)/tegra23x/nvhost_nvenc080.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra23x/nvhost_nvenc080.fw \
+    $(T234_FIRMWARE_PATH)/tegra23x/nvhost_tsec_desc.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra23x/nvhost_tsec_desc.fw \
+    $(T234_FIRMWARE_PATH)/tegra23x/nvhost_tsec_riscv.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/tegra23x/nvhost_tsec_riscv.fw \
+    $(T234_FIRMWARE_PATH)/nvhost_ofa012.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvhost_ofa012.fw \
+    $(T234_FIRMWARE_PATH)/nvhost_vic042.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvhost_vic042.fw \
+    $(T234_FIRMWARE_PATH)/nvpva_020.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvpva_020.fw
